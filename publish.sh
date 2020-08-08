5b0b62027d2835747275b85a5a9f707174d2d02b
nanjingtao <nanjingtao@vipkid.com.cn>
2020-08-08 18:11:16 +0800

    稳定版本号

--- publish.sh
+++ publish.sh
@@ -2,6 +2,14 @@
 
 if [ -d "./dist" ];then
   echo "\033[31m ====dist文件夹已存在=======end====\033[0m"
+  read -p "您是否要重新执行打包命令(y/n): " command
+  if [ "$command" == "y" ];then
+  rm -rf dist
+  exitIfNeed '删除dist文件失败'
+  echo -e "\033[44;37mcommand =====开始build========start=====\033[0m"
+  yarn build
+  exitIfNeed '====Build uboot failed!===='
+  fi
 else
   rm -rf dist
   exitIfNeed '删除dist文件失败'
